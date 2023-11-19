9681e17f7000dbbd6ae74512b1eb346867a27e18
do tuan anh <dotuananh.dta@gmail.com>
2023-11-19 14:40:38 +0700

    C5W2

--- C5 - Sequence Models/Week 2/Emojify/emo_utils.py
+++ C5 - Sequence Models/Week 2/Emojify/emo_utils.py
@@ -1,12 +1,13 @@
 import csv
-import numpy as np
+
 import emoji
-import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+import numpy as np
+import pandas as pd
+
 
 def read_glove_vecs(glove_file):
-    with open(glove_file, 'r') as f:
+    with open(glove_file, "r") as f:
         words = set()
         word_to_vec_map = {}
         for line in f:
@@ -14,7 +15,7 @@ def read_glove_vecs(glove_file):
             curr_word = line[0]
             words.add(curr_word)
             word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
-        
+
         i = 1
         words_to_index = {}
         index_to_words = {}
@@ -24,17 +25,18 @@ def read_glove_vecs(glove_file):
             i = i + 1
     return words_to_index, index_to_words, word_to_vec_map
 
+
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum()
 
 
-def read_csv(filename = 'data/emojify_data.csv'):
+def read_csv(filename="data/emojify_data.csv"):
     phrase = []
     emoji = []
 
-    with open (filename) as csvDataFile:
+    with open(filename) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
 
         for row in csvReader:
@@ -46,71 +48,79 @@ def read_csv(filename = 'data/emojify_data.csv'):
 
     return X, Y
 
+
 def convert_to_one_hot(Y, C):
     Y = np.eye(C)[Y.reshape(-1)]
     return Y
 
 
-emoji_dictionary = {#"0": ":red_heart:",    # :heart: prints a black instead of red heart depending on the font
-                    "0": "\u2764\ufe0f",
-                    "1": ":baseball:",
-                    "2": ":smile:",
-                    "3": ":disappointed:",
-                    "4": ":fork_and_knife:"}
+emoji_dictionary = {  # "0": ":red_heart:",    # :heart: prints a black instead of red heart depending on the font
+    "0": "\u2764\ufe0f",
+    "1": ":baseball:",
+    "2": ":smile:",
+    "3": ":disappointed:",
+    "4": ":fork_and_knife:",
+}
+
 
 def label_to_emoji(label):
     """
     Converts a label (int or string) into the corresponding emoji code (string) ready to be printed
     """
-    return emoji.emojize(emoji_dictionary[str(label)], use_aliases=True)
-              
-    
+    return emoji.emojize(emoji_dictionary[str(label)], language="alias")
+
+
 def print_predictions(X, pred):
     print()
     for i in range(X.shape[0]):
         print(X[i], label_to_emoji(int(pred[i])))
-        
-        
-def plot_confusion_matrix(y_actu, y_pred, title='Confusion matrix', cmap=plt.cm.gray_r):
-    
-    df_confusion = pd.crosstab(y_actu, y_pred.reshape(y_pred.shape[0],), rownames=['Actual'], colnames=['Predicted'], margins=True)
-    
+
+
+def plot_confusion_matrix(y_actu, y_pred, title="Confusion matrix", cmap=plt.cm.gray_r):
+    df_confusion = pd.crosstab(
+        y_actu,
+        y_pred.reshape(
+            y_pred.shape[0],
+        ),
+        rownames=["Actual"],
+        colnames=["Predicted"],
+        margins=True,
+    )
+
     df_conf_norm = df_confusion / df_confusion.sum(axis=1)
-    
-    plt.matshow(df_confusion, cmap=cmap) # imshow
-    #plt.title(title)
+
+    plt.matshow(df_confusion, cmap=cmap)  # imshow
+    # plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(df_confusion.columns))
     plt.xticks(tick_marks, df_confusion.columns, rotation=45)
     plt.yticks(tick_marks, df_confusion.index)
-    #plt.tight_layout()
+    # plt.tight_layout()
     plt.ylabel(df_confusion.index.name)
     plt.xlabel(df_confusion.columns.name)
-    
-    
-    
+
+
 def predict(X, Y, W, b, word_to_vec_map):
     """
     Given X (sentences) and Y (emoji indices), predict emojis and compute the accuracy of your model over the given set.
-    
+
     Arguments:
     X -- input data containing sentences, numpy array of shape (m, None)
     Y -- labels, containing index of the label emoji, numpy array of shape (m, 1)
-    
+
     Returns:
     pred -- numpy array of shape (m, 1) with your predictions
     """
     m = X.shape[0]
     pred = np.zeros((m, 1))
     any_word = list(word_to_vec_map.keys())[0]
-    # number of classes  
-    n_h = word_to_vec_map[any_word].shape[0] 
-    
-    for j in range(m):                       # Loop over training examples
-        
+    # number of classes
+    n_h = word_to_vec_map[any_word].shape[0]
+
+    for j in range(m):  # Loop over training examples
         # Split jth test example (sentence) into list of lower case words
         words = X[j].lower().split()
-        
+
         # Average words' vectors
         avg = np.zeros((n_h,))
         count = 0
@@ -118,7 +128,7 @@ def predict(X, Y, W, b, word_to_vec_map):
             if w in word_to_vec_map:
                 avg += word_to_vec_map[w]
                 count += 1
-        
+
         if count > 0:
             avg = avg / count
 
@@ -126,7 +136,7 @@ def predict(X, Y, W, b, word_to_vec_map):
         Z = np.dot(W, avg) + b
         A = softmax(Z)
         pred[j] = np.argmax(A)
-        
-    print("Accuracy: "  + str(np.mean((pred[:] == Y.reshape(Y.shape[0],1)[:]))))
-    
+
+    print("Accuracy: " + str(np.mean((pred[:] == Y.reshape(Y.shape[0], 1)[:]))))
+
     return pred
